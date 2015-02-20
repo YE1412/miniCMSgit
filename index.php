0e4305443281d7bdca42191e07b1b90f548bf909
navec <navecbatchi@gmail.com>
2015-02-20 12:08:09 +0100

    top

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 	session_start();
 	include('include/head.html');
-	include('include/header.html');
 	include('module/class/View.class.php');
 	include('module/class/User.class.php');
 
 	$view=new View("layout/connexion.html");
 	if(isset($userConnecter)) {
+		include('include/header.html');
 		# code...
 	}else{
 		echo $view->render(array());	
